8169ba1415e154797b71e72d6322a528ac062f38
hanhong <hanhong>
2023-03-15 11:15:06 +0800

    Finish CRUD function

---
+++ src/main/java/com/hanhong/springbootmall/service/impl/ProductServiceImpl.java
@@ -0,0 +1,37 @@
+package com.hanhong.springbootmall.service.impl;
+
+import com.hanhong.springbootmall.dao.ProductDao;
+import com.hanhong.springbootmall.dto.ProductRequest;
+import com.hanhong.springbootmall.model.Product;
+import com.hanhong.springbootmall.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProductServiceImpl implements ProductService {
+
+    @Autowired
+    private ProductDao productDao;
+
+    @Override
+    public Product getProductById(Integer productId) {
+
+        return productDao.getProductById(productId);
+    }
+
+    @Override
+    public Integer createProduct(ProductRequest productRequest) {
+
+        return productDao.createProduct(productRequest);
+    }
+
+    @Override
+    public void updateProduct(Integer productId, ProductRequest productRequest) {
+        productDao.updateProduct(productId, productRequest);
+    }
+
+    @Override
+    public void deleteProductById(Integer productId) {
+        productDao.deleteProductById(productId);
+    }
+}
