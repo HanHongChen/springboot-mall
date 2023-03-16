370cca3661b666e736a119c12ca2e62c110fdca3
hanhong <hanhong>
2023-03-16 11:08:31 +0800

    帳號功能-檢查註冊email、隱藏返回的密碼

--- src/main/java/com/hanhong/springbootmall/service/impl/UserServiceImpl.java
+++ src/main/java/com/hanhong/springbootmall/service/impl/UserServiceImpl.java
@@ -3,12 +3,21 @@ package com.hanhong.springbootmall.service.impl;
 import com.hanhong.springbootmall.dao.UserDao;
 import com.hanhong.springbootmall.dto.UserRegisterRequest;
 import com.hanhong.springbootmall.model.User;
+import com.hanhong.springbootmall.rowmapper.UserRowMapper;
 import com.hanhong.springbootmall.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 @Component
 public class UserServiceImpl implements UserService {
+
+    private final static Logger log = LoggerFactory.getLogger(UserServiceImpl.class);
 
     @Autowired
     private UserDao userDao;
@@ -20,6 +29,15 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public Integer register(UserRegisterRequest userRegisterRequest) {
+        //檢查註冊的email
+        User user = userDao.getUserByEmail((userRegisterRequest.getEmail()));
+
+        if(user != null ){
+            log.warn("該email {} 已經被註冊", userRegisterRequest.getEmail());
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+
+        //註冊帳號
         return userDao.createUser(userRegisterRequest);
     }
 }
