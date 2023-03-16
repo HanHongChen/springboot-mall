e6de7cbd960f73705a7a46a8b1f42daf91b92c5f
hanhong <hanhong>
2023-03-16 11:26:14 +0800

    帳號功能-登入

--- src/main/java/com/hanhong/springbootmall/service/UserService.java
+++ src/main/java/com/hanhong/springbootmall/service/UserService.java
@@ -1,9 +1,11 @@
 package com.hanhong.springbootmall.service;
 
+import com.hanhong.springbootmall.dto.UserLoginRequest;
 import com.hanhong.springbootmall.dto.UserRegisterRequest;
 import com.hanhong.springbootmall.model.User;
 
 public interface UserService {
     User getUserById(Integer userId);
     Integer register(UserRegisterRequest userRegisterRequest);
+    User login(UserLoginRequest userLoginRequest);
 }
