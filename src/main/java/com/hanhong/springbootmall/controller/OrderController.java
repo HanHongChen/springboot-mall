c80bff58313fc0da495d41067dee1859e6137885
chh-3090 <chh.cs12@nycu.edu.tw>
2026-05-01 19:10:38 +0800

    fix: add @Validated

--- src/main/java/com/hanhong/springbootmall/controller/OrderController.java
+++ src/main/java/com/hanhong/springbootmall/controller/OrderController.java
@@ -9,6 +9,7 @@ import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -17,6 +18,7 @@ import javax.validation.constraints.Min;
 import java.util.List;
 
 @RestController
+@Validated
 public class OrderController {
 
     @Autowired
